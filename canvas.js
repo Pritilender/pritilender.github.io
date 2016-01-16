a83861ace8c27f7ee3db4cc50694931335707ccd
Pritilender <sajn_ap@live.com>
2016-01-16 15:26:11 +0100

    Funkcija za racunanje dgr u rad

--- canvas.js
+++ canvas.js
@@ -3,21 +3,28 @@ var ctx = cvs.getContext("2d");
 ctx.fillStyle = "green";
 //ctx.fillRect(10, 10, 100, 100);
 var i = 0;
-var ang = 0;
-var dgr2rad = 0.0873;
+var ang = 1;
 var txt = 0;
+
+var dgr2rad = function(dgr){
+	return dgr*(Math.PI/180);
+}
+
+var portion = function(dgr){
+	return dgr/360*100;
+}
+
 var interval = setInterval(function(){
 	if(i <= 100){
 		ctx.fillRect(10, 10, 100, i);
 		i += 5;
-
-	} else if(ang < 2 * Math.PI){		
+	} else if(ang <= 360){		
 		ctx.clearRect(120, 0, 200, 200);
-		ang += dgr2rad;
 		ctx.beginPath();
-		ctx.arc(200, 60, 50, 0, ang);
+		ctx.arc(200, 60, 50, 0, dgr2rad(ang));
 		ctx.stroke();
-		ctx.fillText(txt++, 195, 60);
+		ctx.fillText(portion(ang).toFixed(2), 195, 60);
+		ang++;
 		console.log(ang);
 	} else {
 		clearInterval(interval);
