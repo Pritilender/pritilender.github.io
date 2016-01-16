167884b82008ada4e984c7464819b9450d58ae3c
Pritilender <sajn_ap@live.com>
2016-01-16 17:00:27 +0100

    Posebna funkcija za crtanje kruga procenata

--- canvas.js
+++ canvas.js
@@ -3,7 +3,6 @@ var ctx = cvs.getContext("2d");
 ctx.fillStyle = "green";
 //ctx.fillRect(10, 10, 100, 100);
 var i = 0;
-var ang = 1;
 var txt = 0;
 
 var dgr2rad = function(dgr){
@@ -14,7 +13,7 @@ var portion = function(dgr){
 	return dgr/360*100;
 }
 
-var interval = setInterval(function(){
+/*var interval = setInterval(function(){
 	if(i <= 100){
 		ctx.fillRect(10, 10, 100, i);
 		i += 5;
@@ -28,4 +27,26 @@ var interval = setInterval(function(){
 	} else {
 		clearInterval(interval);
 	}
-	}, 20);
+	}, 20);*/
+
+var drawPercentage = function(prct, width = 20, ival = 0){
+	ctx.lineWidth = width;
+	var ang = 1;
+	//console.log("Prct: ", prct, " Wdth: ", width, "Ang: ", ang);
+
+	var interval = setInterval(function(){
+		//console.log("Prct: ", prct, " Wdth: ", width, "Ang: ", ang);
+		if(ang <= 360*prct){
+			ctx.clearRect(120, 0, 200, 200);
+			ctx.beginPath();
+			ctx.arc(200, 60, 50, 0, dgr2rad(ang));
+			ctx.stroke();
+			ctx.fillText(portion(ang).toFixed(2), 195, 60);
+			ang++;
+		} else {
+			clearInterval(interval);
+		}
+	}, ival);
+}
+
+drawPercentage(1, 20);
